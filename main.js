fc2b17bba5995bb3975633a24e90f79566174dd8
Bello2288 <bellofattogregg@gmail.com>
2022-09-16 12:05:05 -0400

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
- 
+const Player = function({name} = {}) {
+    this.name = name;
+    this.hand = [];
+  }
+  
+//   const Game = function() {
+//     const player1 = prompt("Enter player one's name: ");
+//     const player2 = prompt("Enter player two's name: ");
+  
+//     this.player1 = new Player({name: player1});
+//     this.player2 = new Player({name: player2});
+  
+//     this.deck = new Deck();
+//   }
+  
+  const game = new Game();
